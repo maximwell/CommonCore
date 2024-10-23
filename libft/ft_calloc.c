9a57f707c186ad520cb0c0ce35ec30aa24e67171
Maxim Marhic <mmarhic@c1r8s4.42luxembourg.lu>
2024-10-23 22:17:12 +0200

    libft

---
+++ libft/ft_calloc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmarhic <mmarhic@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 01:25:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/23 14:36:27 by mmarhic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	unsigned char	*ptr;
+	size_t			total_size;
+	size_t			i;
+
+	if (num && size > SIZE_MAX / num)
+		return (NULL);
+	total_size = num * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < total_size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
