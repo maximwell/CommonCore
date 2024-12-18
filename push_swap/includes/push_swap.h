23b553edaf174b2c2be257147e5f0165f183324c
Max Mar <maxmarhic@jymac.com>
2024-12-18 01:47:49 +0100

    oe

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 04:58:14 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/06 04:58:14 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+// trouver si c'est un long ou un int
+
+// ajouter 2 atoi
+
+// ajouter le radix sort
+
+
+
+
+
+typedef struct s_list
+{
+	int				index;
+	int				nombre;
+	struct s_list	*next;
+}					t_list;
+
+#endif
